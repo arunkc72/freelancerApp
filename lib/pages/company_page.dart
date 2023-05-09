36b37b7c49668ca2658643933a88fa1ea33699fc
unknown <sumandarji2479@gmail.com>
2023-05-09 07:26:34 +0545

    check

--- lib/pages/company_page.dart
+++ lib/pages/company_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+
 class CompanyPage extends StatefulWidget {
-  const CompanyPage({ Key? key }) : super(key: key);
+  const CompanyPage({Key? key}) : super(key: key);
 
   @override
   _CompanyPageState createState() => _CompanyPageState();
@@ -10,7 +11,21 @@ class _CompanyPageState extends State<CompanyPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/DarkThemeBackground1-01 3.png"),
+            fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        // appBar: AppBar(
+        //   backgroundColor: Colors.transparent.withOpacity(0.2),
+        // ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text("hello there"), Text("hello there")],
+        ),
+      ),
     );
   }
 }
